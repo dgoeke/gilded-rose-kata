f499b42507b5cb69d33fd7bf116282babbf22748
David Goeke <dg@dgoeke.io>
2016-07-01 21:28:01 -0700

    My solution to the kata

--- clojure/spec/gilded_rose/core_spec.clj
+++ clojure/spec/gilded_rose/core_spec.clj
@@ -1,6 +1,82 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer :all]))
 
-(describe "gilded rose"
-          (it "TODO - write tests later"))
+(defn test-item
+  "Construct a mock item with the desired traits"
+  [sell-in quality & traits]
+  (apply with-traits (item "Test item" sell-in quality) traits))
+
+(defn quality-diff
+  "Return the quality difference when updating just this item"
+  [item]
+  (let [old-quality (:quality item)
+        new-quality (:quality (first (update-quality [item])))]
+    (- new-quality old-quality)))
+
+(defn as-expired [item]
+  (assoc item :sell-in -1))
+
+(defn as-conjured [item]
+  (update item :traits conj conjured))
+
+(let [hq-item  (test-item 10 90 standard)
+      lq-item  (test-item 10 -5 standard)
+      sulfuras (test-item 10 10 permanent)]
+  (describe "item quality should be within limits"
+            (it "should not be increased beyond 50"
+                (should (= -40 (quality-diff hq-item))))
+            (it "but Sulfuras can go over 50"
+                (should (zero? (quality-diff sulfuras))))
+            (it "most items can't go below zero"
+                (should (= 5 (quality-diff lq-item))))))
+
+(let [item (test-item 10 5 standard)]
+  (describe "standard items"
+            (it "quality reduced by 1 (base)"
+                (should (= -1 (quality-diff item))))
+            (it "quality reduced by 2 (expired)"
+                (should (= -2 (quality-diff (as-expired item)))))
+            (it "quality reduced by 2 (conjured)"
+                (should (= -2 (quality-diff (as-conjured item)))))
+            (it "quality reduced by 4 (conjured, expired)"
+                (should (= -4 (quality-diff (as-conjured (as-expired item))))))))
+
+(let [item (test-item 10 5 accruing standard)]
+  (describe "accruing items"
+            (it "quality increases daily"
+                (should (= 1 (quality-diff item))))
+            (it "quality increases by 1 (expired)"
+                (should (= 1 (quality-diff (as-expired item)))))
+            (it "quality increases by 1 (conjured)"
+                (should (= 1 (quality-diff (as-conjured item)))))
+            (it "quality increases by 1 (conjured, expired)"
+                (should (= 1 (quality-diff (as-expired (as-conjured item))))))))
+
+(let [item (test-item 10 5 permanent)]
+  (describe "permanent items"
+            (it "don't decrease in quality (base)"
+                (should (zero? (quality-diff item))))
+            (it "don't decrease in quality (expired)"
+                (should (zero? (quality-diff (as-expired item)))))
+            (it "don't decrease in quality (conjured)"
+                (should (zero? (quality-diff (as-conjured item)))))
+            (it "don't decrease in quality (conjured)"
+                (should (zero? (quality-diff (as-conjured (as-expired item))))))))
+
+(let [item            (test-item 15 20 ticketmaster standard)
+      item-danger     (assoc item :sell-in 10)
+      item-d-d-danger (assoc item :sell-in 5)]
+  (describe "backstage passes"
+            (it "increases normally far from the concert"
+                (should (= 1 (quality-diff item))))
+            (it "increases by 2 with <= 10 days left"
+                (should (= 2 (quality-diff item-danger))))
+            (it "increases by 2 with <= 10 days left (conjured)"
+                (should (= 2 (quality-diff (as-conjured item-danger)))))
+            (it "increases by 3 with <= 5 days left"
+                (should (= 3 (quality-diff item-d-d-danger))))
+            (it "increases by 3 with <= 5 days left (conjured)"
+                (should (= 3 (quality-diff (as-conjured item-d-d-danger)))))
+            (it "is worthless after the concert"
+                (should (= -20 (quality-diff (as-expired item)))))))
--- clojure/src/gilded_rose/core.clj
+++ clojure/src/gilded_rose/core.clj
@@ -1,44 +1,100 @@
 (ns gilded-rose.core)
 
-(defn update-quality [items]
-  (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-        (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (inc (inc (:quality item)))})
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (inc (inc (inc (:quality item))))})
-            (if (< (:quality item) 50)
-              (merge item {:quality (inc (:quality item))})
-              item)))
-      (< (:sell-in item) 0)
-        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
-          (merge item {:quality 0})
-          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-            (merge item {:quality (- (:quality item) 2)})
-            item))
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+(def max-quality 50)  ; calling (limit-quality ...) applies these
+(def min-quality 0)
+
+(defprotocol ItemTrait
+  "Item traits may modify the rate of quality and sell-in decay."
+  (adjust-quality [this item q-delta] "Given a quality adjustment, return a new one.")
+  (adjust-sell-in [this item sell-in-delta] "Given a sell-in adjustment, return a new one."))
+
+(defn with-traits
+  "Apply the given traits to this item"
+  [item & traits]
+  (assoc item :traits traits))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
 
+(defn compose-item-traits
+  "Convert an items seq of traits into a function by composing them all together"
+  [item f]
+  (if-let [traits (:traits item)]
+    (->> traits
+         (map (fn [trait] (partial f trait item)))
+         (apply comp))
+    identity))
+
+(defn adjust-item
+  "Modify an item by running `trait-fn` in all traits and adding the reuslt to `field`"
+  [item field trait-fn]
+  (let [delta-fn (compose-item-traits item trait-fn)
+        delta    (delta-fn 0)]
+    (update item field (partial + delta))))
+
+(defn adjust-item-quality
+  "Run all `adjust-quality` traits and update the :quality field"
+  [item]
+  (adjust-item item :quality adjust-quality))
+
+(defn adjust-item-sell-in
+  "Run all `adjust-sell-in` traits and update the :sell-in field"
+  [item]
+  (adjust-item item :sell-in adjust-sell-in))
+
+(defn update-quality [inventory]
+  (->> inventory
+       (map adjust-item-sell-in)
+       (map adjust-item-quality)))
+
+(defn limit-quality
+  "Alter q-delta so it doesn't take quality out of bounds"
+  [item q-delta]
+  (let [quality     (:quality item)
+        new-quality (max min-quality (min max-quality (+ quality q-delta)))]
+    (- new-quality quality)))
+
+;; Traits!
+(def standard
+  (reify ItemTrait
+    (adjust-quality [_ item _]
+      (limit-quality item (if (neg? (:sell-in item)) -2 -1)))
+    (adjust-sell-in [_ _ _] -1)))
+
+(def conjured
+  (reify ItemTrait
+    (adjust-quality [_ item delta]
+      (limit-quality item (if (neg? delta) (* 2 delta) delta)))
+    (adjust-sell-in [_ _ delta] delta)))
+
+(def accruing
+  (reify ItemTrait
+    (adjust-quality [_ item _]
+      (limit-quality item (if (>= (:quality item) max-quality) 0 1)))
+    (adjust-sell-in [_ _ delta] delta)))
+
+(def permanent
+  (reify ItemTrait
+    (adjust-quality [_ _ _] 0)
+    (adjust-sell-in [_ _ _] 0)))
+
+(def ticketmaster
+  (reify ItemTrait
+    (adjust-quality [_ item delta]
+      (let [{:keys [sell-in quality]} item]
+        (limit-quality item
+         (cond (neg? sell-in)  (- 0 quality) ; lower back to zero
+               (<= sell-in 5)  3
+               (<= sell-in 10) 2
+               :else           1))))
+
+    (adjust-sell-in [_ _ delta] delta)))
+
 (defn update-current-inventory[]
-  (let [inventory 
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand Of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+  (let [inventory [(with-traits (item "+5 Dexterity Vest" 10 20) standard)
+                   (with-traits (item "Aged Brie" 2 0) accruing standard)
+                   (with-traits (item "Elixir of the Mongoose" 5 7) standard)
+                   (with-traits (item "Sulfuras, Hand Of Ragnaros" 0 80) permanent)
+                   (with-traits (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
+                     ticketmaster standard)]]
+    (update-quality inventory)))
